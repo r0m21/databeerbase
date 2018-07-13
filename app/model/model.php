14bdf324dab9083804bbe435b5ab6dac8ff57ca4
erickb21 <erick.beaugendre@gmail.com>
2018-07-13 16:34:02 +0200

    ajout link nouislider.css dans layout + fichier nouislider.css nouislider.js

--- app/model/model.php
+++ app/model/model.php
@@ -28,25 +28,25 @@ class Beers extends Model {
 }
 
  // ******** recherche par degrés categories styles nom de bière
-// degBeermin => degrés / degBeermax => degrés / nom => recherche bière / nationalite => nationalite des bières / styleBeer => style de bière
+// degBeermin => degrés / degBeerMax => degrés / nom => recherche bière / nationalite => nationalite des bières / styleBeer => style de bière
    public function search() {
 
    $sBeer="%".strip_tags($_POST["nom"])."%";
    $sCat=strip_tags($_POST["nationalite"]);
    $sStyle="%".strip_tags($_POST["styleBeer"])."%";
-   $sDegMin=strip_tags($_POST["degBeermin"]);
-   $sDegMax=strip_tags($_POST["degBeermax"]);
+   $sDegMin=strip_tags($_POST["degBeerMin"]);
+   $sDegMax=strip_tags($_POST["degBeerMax"]);
 
    
         $db = Database::getInstance();
-        $sql = "SELECT * FROM  `beer`, `categories`, `style` WHERE cat_BEE = id_CAT AND style_BEE=id_STY and name_BEE like :nom AND cat_BEE= :nationalite AND name_STY like :styleBeer AND deg_BEE >= :degBeermin AND deg_BEE <= :degBeermax ORDER BY name_BEE ";
+        $sql = "SELECT * FROM  `beer`, `categories`, `style` WHERE cat_BEE = id_CAT AND style_BEE=id_STY and name_BEE like :nom AND cat_BEE= :nationalite AND name_STY like :styleBeer AND deg_BEE >= :degBeerMin AND deg_BEE <= :degBeerMax ORDER BY name_BEE ";
         $stmt = $db->prepare($sql);
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
         $stmt->bindValue(':nom', $sBeer, PDO::FETCH_ASSOC);
         $stmt->bindValue(':nationalite', intval($sCat), PDO::FETCH_ASSOC);
         $stmt->bindValue(':styleBeer', $sStyle, PDO::FETCH_ASSOC);
-        $stmt->bindValue(':degBeermin', floatval($sDegMin), PDO::FETCH_ASSOC);
-        $stmt->bindValue(':degBeermax', floatval($sDegMax), PDO::FETCH_ASSOC);
+        $stmt->bindValue(':degBeerMin', floatval($sDegMin), PDO::FETCH_ASSOC);
+        $stmt->bindValue(':degBeerMax', floatval($sDegMax), PDO::FETCH_ASSOC);
         $stmt->execute();
         return $stmt->fetchAll();
 }
